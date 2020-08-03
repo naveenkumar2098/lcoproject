d9049c7b96b10e78cc002e21e589f1a90ddbd5c5
naveenkumar2098 <kumarn.na20@gmail.com>
2020-08-04 02:17:13 +0530

    Added category buttons

--- src/components/Cart/CourseCart.js
+++ src/components/Cart/CourseCart.js
@@ -21,11 +21,44 @@ export default class CourseCart extends Component {
             mycategories : getCategory(props.courses.edges)
         }
     }
+
+    btnClicked = category => {
+        let allCourses = [...this.state.courses]
+
+        if(category === "all"){
+            this.setState(() => {
+                return {mycourses : allCourses}
+            })
+        }else{
+            let filteredCourses = allCourses.filter(({node}) => node.category === category)
+            this.setState(() => {
+                return {mycourses : filteredCourses}
+            })
+        }
+    }
+    
     render() {
         return (
             <section className="py-5">
                 <div className="container">
                     <Heading title="Courses"/>
+                    <div className="row my-3">
+                        <div className="col-10 mx-auto text-center">
+                            {
+                                this.state.mycategories.map((category, index) => {
+                                    return(
+                                        <button type="button"
+                                         className="btn btn-info m-3 px-3" 
+                                         key={index} 
+                                         onClick={() => {
+                                            this.btnClicked(category)
+                                        }}
+                                        >{category}</button>
+                                    )
+                                })
+                            }
+                        </div>
+                    </div>
                     <div className="row">
                         {
                             this.state.mycourses.map(({node})=>{
